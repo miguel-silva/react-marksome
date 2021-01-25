533411be46d980d69e2e2f2df65c1b9a377800aa
Miguel Silva <miguel.smsilva@gmail.com>
2021-01-25 00:10:04 +0000

    update pre-commit hook and snapshots

--- test/Marksome.test.tsx
+++ test/Marksome.test.tsx
@@ -7,78 +7,82 @@ test('renders mixed inline styles as expected', () => {
   const { asFragment } = render(<Marksome text={mixedInlineStylesFixture} />);
 
   expect(asFragment()).toMatchInlineSnapshot(`
-		<DocumentFragment>
-		  <span>
-		    This is 
-		    <em>
-		      emphasized
-		    </em>
-		    , 
-		    <strong>
-		      strong
-		    </strong>
-		    ,  
-		    <em>
-		      emphasized with 
-		    </em>
-		    <strong>
-		      a break of strong
-		    </strong>
-		    <em>
-		       in the middle
-		    </em>
-		    , 
-		    <strong>
-		      strong with 
-		    </strong>
-		    <em>
-		      a break of emphasized
-		    </em>
-		    <strong>
-		       in the middle
-		    </strong>
-		    , 
-		    <strong>
-		      <em>
-		        strong within
-		      </em>
-		    </strong>
-		    <em>
-		       emphasized
-		    </em>
-		    , 
-		    <strong>
-		      <em>
-		        emphasized within
-		      </em>
-		       strong
-		    </strong>
-		     and 
-		    <strong>
-		      <em>
-		        both
-		      </em>
-		    </strong>
-		    !
-		  </span>
-		</DocumentFragment>
-	`);
+    		<DocumentFragment>
+    		  <span>
+    		    This is 
+    		    <em>
+    		      emphasized
+    		    </em>
+    		    , 
+    		    <strong>
+    		      strong
+    		    </strong>
+    		    ,  
+    		    <em>
+    		      emphasized with 
+    		    </em>
+    		    <strong>
+    		      a break of strong
+    		    </strong>
+    		    <em>
+    		       in the middle
+    		    </em>
+    		    , 
+    		    <strong>
+    		      strong with 
+    		    </strong>
+    		    <em>
+    		      a break of emphasized
+    		    </em>
+    		    <strong>
+    		       in the middle
+    		    </strong>
+    		    , 
+    		    <strong>
+    		      <em>
+    		        strong within
+    		      </em>
+    		    </strong>
+    		    <em>
+    		       emphasized
+    		    </em>
+    		    , 
+    		    <strong>
+    		      <em>
+    		        emphasized within
+    		      </em>
+    		       strong
+    		    </strong>
+    		     and 
+    		    <strong>
+    		      <em>
+    		        both
+    		      </em>
+    		    </strong>
+    		    !
+    		  </span>
+    		</DocumentFragment>
+  	`);
 });
 
 test('renders reference link as expected', () => {
   const { asFragment } = render(<Marksome {...referenceLinkFixture} />);
 
   expect(asFragment()).toMatchInlineSnapshot(`
-		<DocumentFragment>
-		  <span>
-		    For wider markdown support, check 
-		    <a
-		      href="https://www.npmjs.com/package/markdown-to-jsx"
-		    >
-		      markdown-to-jsx
-		    </a>
-		     out!
-		  </span>
-		</DocumentFragment>
-	`);
+    <DocumentFragment>
+      <span>
+        For wider markdown support, 
+        <a
+          href="https://www.npmjs.com/package/markdown-to-jsx"
+        >
+          check 
+          <strong>
+            markdown-to-jsx
+          </strong>
+           out
+        </a>
+        !
+      </span>
+    </DocumentFragment>
+  `);
 });
